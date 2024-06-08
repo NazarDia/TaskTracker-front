d3fd60183d8ebf85c1591361c10fdd4e514da776
Nazar Dia <nazar.dia@gmail.com>
2024-06-08 19:54:14 +0200

    update welcome page

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,33 +1,31 @@
 import s from './WelcomePage.module.css';
-// import icons from '../../images/sprite/icons.svg';
+import icon from '../../images/sprite/sprite-icon.svg';
 
 import { Link } from 'react-router-dom';
 
 export default function WelcomePage() {
   return (
     <div className={s.welcomeContainer}>
-      <div className={s.welcomeLogo}>
-        <img
-          src="/src/images/logo/logo-mob.png"
-          alt="Logo"
-          className={s.logo}
-        />
-        <div className={s.welcomeText}>
-          <span className={s.logoContainer}>
-            {/* <svg className={s.logoIcon}>
-              <use xlinkHref={`${icons}#icon-lightning`} />
-            </svg> */}
+      <div className={s.welcomeHeader}>
+        <div className={s.welcomeImage}></div>
+        <div className={s.welcomeNameWrapper}>
+          <span className={s.welcomeIconWrapper}>
+            <svg className={s.welcomeIcon} width={16} height={20}>
+              <use href={`${icon}#icon-welcome`}></use>
+            </svg>
           </span>
-          <p className={s.logoItem}>Task Pro</p>
+          <h1 className={s.welcomeName}>Task Pro</h1>
         </div>
       </div>
-      <p className={s.mainSlogan}>
-        Supercharge your productivity and take control of your tasks with Task
-        Pro - Do not wait, start achieving your goals now!
+      <p className={s.welcomeSlogan}>
+        Supercharge your productivity and take control
+        <br className={s.tabletBreak} /> of your tasks with Task
+        <br className={s.mobileBreak} /> Pro - Don&apos;t wait, start
+        <br className={s.tabletBreak} /> achieving your goals now!
       </p>
       <div className={s.mainButtons}>
         <Link to="/auth/register">
-          <button className={s.registerButton}>Register</button>
+          <button className={s.registerButton}>Registration</button>
         </Link>
         <Link to="/auth/login">
           <button className={s.loginButton}>Log In</button>
