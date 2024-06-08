64c6756fe34deb808f9d92d4f8d201f31c37b614
Nadiia <nadiahecko@gmail.com>
2024-06-08 12:43:38 +0300

    add redux-board

---
+++ src/redux/boards/operations.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchBoards = createAsyncThunk(
+  'boards/fetchBoards',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/boards');
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const addBoard = createAsyncThunk(
+  'boards/addBoard',
+  async (board, thunkAPI) => {
+    try {
+      const response = await axios.post('/boards', board);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const editBoardById = createAsyncThunk(
+  'boards/editBoardById',
+  async (params, thunkAPI) => {
+    try {
+      const { boardId, updatedData } = params;
+      const response = await axios.put(`/boards/${boardId}`, updatedData);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+export const deleteBoard = createAsyncThunk(
+  'boards/deleteBoard',
+  async (boardId, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/boards/${boardId}`);
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
+
+// export const getBoardById = createAsyncThunk(
+//   "boards/getBoardById",
+//   async (boardId, thunkAPI) => {
+//     try {
+//       const response = await axios.get(`/boards/${boardId}`);
+//       return response.data;
+//     } catch (e) {
+//       return thunkAPI.rejectWithValue(e.message);
+//     }
+//   }
+// );
