614aae57d677f8ef4eb7509f9e0b17858ebdd268
Aleksandr-Reznichenko <reznichenko.aleksandr.v@gmail.com>
2024-06-08 13:58:15 +0300

    Add HomePage and Error component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,25 +1,60 @@
-// import s from './HomePage.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchBoards } from '../../redux/boards/operations';
+import { selectIsLoading, selectError } from '../../redux/boards/selectors';
+
+import Loader from '../../components/Loader/Loader';
+import ErrorMessage from '../../components/Error/Error';
+
 import Header from '../../components/Header/Header';
 import Sidebar from '../../components/SideBar/SideBar';
-import { useState } from 'react';
+import HeaderDashboard from '../../components/HeaderDashboard/HeaderDashboard';
 
-const HomePage = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+import s from './HomePage.module.css';
 
-  const handleMenuClick = () => {
-    setIsSidebarOpen(!isSidebarOpen);
-  };
+export default function HomePage() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const errorMessage = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchBoards());
+  }, [dispatch]);
 
   return (
-    <div>
-      <Header openMenu={handleMenuClick} />
-      {isSidebarOpen && <Sidebar />}
-      <main>
-        {/* Add your main page content here */}
-        <h1>Welcome to HomePage</h1>
-      </main>
+    <div className={s.container}>
+      <Sidebar />
+      <div className={s.mainContent}>
+        <Header />
+
+        <div className={s.loaderError}>
+          {isLoading && <Loader />}
+          {errorMessage && <ErrorMessage />}
+        </div>
+
+        {!isLoading && !errorMessage && <HeaderDashboard />}
+      </div>
     </div>
   );
-};
+}
 
-export default HomePage;
+///////////////////////////////////////////////////////////////////
+
+// export default function HomePage() {
+//   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+//   const handleMenuClick = () => {
+//     setIsSidebarOpen(!isSidebarOpen);
+//   };
+
+//   return (
+//     <div className={s.container}>
+//       <Header openMenu={handleMenuClick} />
+//       {isSidebarOpen && <Sidebar />}
+//       <main>
+//         {/* Add your main page content here */}
+//         <h1>Welcome to HomePage</h1>
+//       </main>
+//     </div>
+//   );
+// }
