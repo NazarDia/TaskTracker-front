b0b4c631826556d07a9dfb6f01aa29f8d7740cac
Andrii Honcharuk <goncharukam@gmail.com>
2024-06-10 19:31:37 +0300

    add popUpAddColums component add and fix selectors

---
+++ src/redux/columns/selectors.js
@@ -0,0 +1,5 @@
+export const selectColumns = state => state.columns.items;
+
+export const selectColumnsLoading = state => state.columns.loading;
+
+export const selectColumnsError = state => state.columns.error;
