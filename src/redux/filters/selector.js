1bc9c062a5bbc31ec720c65e3ad7d77227516f3e
OlVin69 <aviniichuk@gmail.com>
2024-06-12 00:28:27 +0300

    update EditCard.css and add selectors and slice for filter

---
+++ src/redux/filters/selector.js
@@ -0,0 +1,27 @@
+import { createSelector } from '@reduxjs/toolkit';
+import {selectCards} from "../cards/selectors";
+
+// Селектор для отримання фільтра пріоритету
+export const selectPriorityFilter = state => state.filters.priority;
+
+// Мапа кольорів до значень пріоритетів
+const priorityColorMap = {
+  none: 'rgba(255, 255, 255, 0.3)',
+  low: '#8fa1d0',   
+  medium: '#e09cb5', 
+  high: '#bedbb0',   
+};
+
+// Селектор для фільтрації карток за кольором (пріоритетом)
+export const selectFilteredCards = createSelector(
+  [selectCards, selectPriorityFilter],
+  (cards, priorityFilter) => {
+    if (priorityFilter === 'all') {
+      return cards;
+    }
+
+    const color = priorityColorMap[priorityFilter];
+
+    return cards.filter(card => card.color === color);
+  }
+);
