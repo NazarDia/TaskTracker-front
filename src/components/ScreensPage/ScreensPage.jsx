7f4f362a0c6082bb22eb79117480844d3759fb6e
NataOz <merlincat.no@gmail.com>
2024-06-14 13:24:41 +0300

    Set First Board as Default
    додала логіку, щоб відображалась перша борда при логіні

--- src/components/ScreensPage/ScreensPage.jsx
+++ src/components/ScreensPage/ScreensPage.jsx
@@ -2,23 +2,33 @@ import { useDispatch, useSelector } from 'react-redux';
 import HeaderDashboard from '../HeaderDashboard/HeaderDashboard';
 import MainDashboard from '../MainDashboard/MainDashboard';
 import s from './ScreensPage.module.css';
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { getBoardByID } from '../../redux/boards/operations';
 
 const ScreensPage = () => {
   const dispatch = useDispatch();
+  const boards = useSelector(state => state.boards.boards.items);
   const activeBoardId = useSelector(state => state.boards.boards.current._id);
   const activeBoard = useSelector(state =>
     state.boards.boards.items.find(board => board._id === activeBoardId)
   );
-  const previousBoardId = useRef(activeBoardId);
+
+  const [defaultBoardSet, setDefaultBoardSet] = useState(false);
+
+  // }, [dispatch, activeBoardId, boards]);
 
   useEffect(() => {
-    if (activeBoardId && previousBoardId.current !== activeBoardId) {
-      previousBoardId.current = activeBoardId;
+    if (!defaultBoardSet && boards.length > 0) {
+      if (!activeBoardId) {
+        const defaultBoardId = boards[0]._id;
+        dispatch(getBoardByID(defaultBoardId));
+      }
+      setDefaultBoardSet(true);
+    } else if (activeBoardId && !defaultBoardSet) {
       dispatch(getBoardByID(activeBoardId));
+      setDefaultBoardSet(true);
     }
-  }, [dispatch, activeBoardId]);
+  }, [dispatch, activeBoardId, boards, defaultBoardSet]);
 
   return (
     <main className={s.container}>
