e13b9a5156aa24aabe7c2c5223a54cfba9f43363
Nazar Dia <nazar.dia@gmail.com>
2024-06-15 19:51:35 +0200

    add validation

--- src/Schemas/schema.js
+++ src/Schemas/schema.js
@@ -4,9 +4,10 @@ export const registerUserSchema = yup.object().shape({
   name: yup
     .string()
     .trim()
-    .min(3, 'Too short!')
-    .max(50, 'Too long!')
-    .required('Required'),
+    .matches(/^[a-zA-Z0-9]+$/, 'Only alphanumeric characters are allowed')
+    .min(2, 'Too short!')
+    .max(32, 'Too long!')
+    .required('Required, please insert your name'),
   email: yup
     .string()
     .trim()
@@ -15,14 +16,19 @@ export const registerUserSchema = yup.object().shape({
   password: yup
     .string()
     .trim()
-    .min(4, 'Too short!')
+    .min(8, 'Too short!')
+    .max(64, 'Too long!')
+    .matches(
+      /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?!.*\s).{8,64}$/,
+      'A-Z, 0-9, one special character, 8-64 characters long'
+    )
     .required('Required, please insert the password'),
 });
 
 export const loginUserSchema = yup.object().shape({
   email: yup
     .string()
-    .email('Enter correct email example@domain.com')
+    .email('Enter correct email: example@domain.com')
     .required('Required, please insert the email'),
   password: yup.string().required('Required, please insert the password'),
 });
