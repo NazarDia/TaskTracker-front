1816ce0afa93c7d9c61474ea65523f709a94571a
Liubov2909 <ostapcuk2909@gmail.com>
2024-06-16 12:40:45 +0300

    add schema editprofile and avatar

--- src/Schemas/schema.js
+++ src/Schemas/schema.js
@@ -32,3 +32,27 @@ export const loginUserSchema = yup.object().shape({
     .required('Required, please insert the email'),
   password: yup.string().required('Required, please insert the password'),
 });
+
+export const editProfileSchema = yup.object().shape({
+  name: yup
+    .string()
+    .trim()
+    .matches(/^[a-zA-Z0-9]+$/, 'Only alphanumeric characters are allowed')
+    .min(2, 'Too short!')
+    .max(32, 'Too long!'),
+  email: yup
+    .string()
+    .trim()
+    .email(),
+  password: yup
+    .string()
+    .trim()
+    .min(8, 'Too short!')
+    .max(64, 'Too long!')
+    .matches(
+      /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?!.*\s).{8,64}$/,
+      'A-Z, 0-9, one special character, 8-64 characters long'
+    )
+    
+});
+
