d57de8bcc77eecb858520472633154435a16e411
NataOz <merlincat.no@gmail.com>
2024-06-18 02:22:19 +0300

    add styles for diff devices

--- src/components/StartBoard/StartBoard.jsx
+++ src/components/StartBoard/StartBoard.jsx
@@ -1,12 +1,14 @@
 import s from './StartBoard.module.css';
+
 const StartBoard = () => {
   return (
     <div className={s.startWrapper}>
       <div className={s.textInner}>
-        Before starting your project, it is essential to create a board to
-        visualize and track all the necessary tasks and milestones. This board
-        serves as a powerful tool to organize the workflow and ensure effective
-        collaboration among team members.
+        Before starting your project, it is essential&#8194;
+        <span className={s.link}>to create a board</span>
+        &#8194;to visualize and track all the necessary tasks and milestones.
+        This board serves as a powerful tool to organize the workflow and ensure
+        effective collaboration among team members.
       </div>
     </div>
   );
